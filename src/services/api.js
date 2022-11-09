c7d920ad3a0e06401b900db2f59f07c4a269602e
Rmdeaguiar <rafael.aguiar91@gmail.com>
2022-11-09 19:21:13 -0300

    ajustando api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://sualistadetarefas.herokuapp.com/',
     timeout: 100000,
     headers: {
         'Content-Type': 'application/json',
